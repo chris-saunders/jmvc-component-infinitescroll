c11108cf338dc3a2daa0d3b0c9438f6efb177576
Chris Saunders - Home Laptop <slapheadted@gmail.com>
2013-05-29 22:55:49 +0100

    Foundations in place with click handler and external EJS string

--- infinitescroll.js
+++ infinitescroll.js
@@ -1,7 +1,37 @@
 steal(
-	'./infinitescroll.css', 			// application CSS file
-	'./models/models.js',		// steals all your models
-	'./fixtures/fixtures.js',	// sets up fixtures for your models
-	function(){					// configure your application
-		
-	})
+    './infinitescroll.css',             // application CSS file
+    './models/models.js',       // steals all your models
+    './fixtures/fixtures.js',   // sets up fixtures for your models
+    'jquery/class',
+    'jquery/model',
+    'jquery/model/list',
+    'jquery/controller',
+    'jquery/controller/view',
+    'jquery/view/ejs',
+    function(){                 // configure your application
+        $.Controller('Infinitescroll',
+        {
+
+        }, {
+            init: function() {
+                var self = this;
+                if (!this.options.limit || !this.options.model || !this.options.template) {
+                    throw new Error('Must provide integer for `limit` AND JMVC Model for `model` AND EJS string for `template`.');
+                }
+            },
+
+            "click": function() {
+                this.render();
+            },
+
+            render: function() {
+                var self = this;
+                this.options.model.findAll({ offset: 3 }, function( users ) {
+                    users.each(function(index, model) {
+                        self.element.find('table')
+                            .append(new $.EJS({ text: self.options.template }).render({ model: model }));
+                    });
+                })
+            }
+        });
+    })
